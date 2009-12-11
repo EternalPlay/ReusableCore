850900d010d41317ec7e844fe60733a1fdf4cded
jdubba <jdubba@jdubba.net>
2009-12-11 08:58:31 -0600

    Started implementation of configuration management objects.

---
+++ EternalPlay.ReusableCore/UnitTest/UserConfigTest.cs
@@ -0,0 +1,140 @@
+﻿using EternalPlay.ReusableCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+namespace EternalPlay.ReusableCore.UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for UserConfigTest and is intended
+    ///to contain all UserConfigTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class UserConfigTest {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        #region Constructor Tests
+        /// <summary>
+        ///A test for UserConfig Constructor
+        ///</summary>
+        [TestMethod()]
+        public void UserConfigConstructorTest() {
+            string applicationName = "Core Unit Test";
+            string version = "1.0.0.0";
+            
+            UserConfig target = new UserConfig(applicationName, version);
+            Assert.IsNotNull(target);
+        }
+        #endregion
+
+        #region Property Tests
+        /// <summary>
+        ///A test for ApplicationName
+        ///</summary>
+        [TestMethod()]
+        public void UserConfigApplicationNameTest() {
+            string applicationName = "Core Unit Test";
+            string version = "1.0.0.0";
+
+            UserConfig target = new UserConfig(applicationName, version);
+            string actual = target.ApplicationName;
+
+            Assert.AreEqual(applicationName, actual);
+
+        }
+
+        /// <summary>
+        ///A test for ConfigurationFilePath
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EternalPlay.ReusableCore.dll")]
+        public void UserConfigConfigurationFilePathTest() {
+            string applicationName = "Core Unit Test";
+            string version = "1.0.0.0";
+
+            UserConfig_Accessor target = new UserConfig_Accessor(applicationName, version);
+            string actual = target.ConfigurationFilePath;
+            string expected = Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName), string.Format("{0}.{1}.config", applicationName, version));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        /// <summary>
+        ///A test for Version
+        ///</summary>
+        [TestMethod()]
+        public void UserConfigVersionTest() {
+            string applicationName = "Core Unit Test";
+            string version = "1.0.0.0";
+
+            UserConfig target = new UserConfig(applicationName, version);
+            string actual = target.Version;
+
+            Assert.AreEqual(version, actual);
+        }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        ///A test for CreateConfigurationFilePath
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("EternalPlay.ReusableCore.dll")]
+        public void UserConfigCreateConfigurationFilePathTest() {
+            string applicationName = "Core Unit Test";
+            string version = "1.0.0.0";
+            string expected = Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName), string.Format("{0}.{1}.config", applicationName, version));
+            string actual = UserConfig_Accessor.CreateConfigurationFilePath(applicationName, version);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
---
+++ EternalPlay.ReusableCore/UserConfig.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace EternalPlay.ReusableCore {
+    /// <summary>
+    /// A simple to use object heirarchy for managing user configuration files for desktop applications.
+    /// </summary>
+    public sealed class UserConfig {
+        #region Fields
+        private string _applicationName;
+        private string _version;
+        private string _configurationFilePath;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructs the user configuration based on the given application name and version number.
+        /// </summary>
+        /// <param name="applicationName"></param>
+        /// <param name="version"></param>
+        public UserConfig(string applicationName, string version) {
+            _applicationName = applicationName;
+            _version = version;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the name of the application the configuration represents.
+        /// </summary>
+        public string ApplicationName {
+            get {
+                return _applicationName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the version of the application the configuration represents.
+        /// </summary>
+        public string Version {
+            get {
+                return _version;
+            }
+        }
+
+        /// <summary>
+        /// Lazy loaded internal property for the path to the file that holds configuration data
+        /// </summary>
+        private string ConfigurationFilePath {
+            get {
+                return _configurationFilePath ?? (_configurationFilePath = CreateConfigurationFilePath(_applicationName, _version));
+            }
+        }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Constructs a proper configuration file path based on the given application name and version.
+        /// </summary>
+        /// <param name="applicationName">Name of the application being configured.</param>
+        /// <param name="version">Version of the application being configured.</param>
+        /// <returns>Full path name to the application configuration file.</returns>
+        private static string CreateConfigurationFilePath(string applicationName, string version) {
+            return Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), applicationName), string.Format(Constants.ConfigFileNameFormat, applicationName, version));
+        }
+        #endregion
+
+        #region Nested Types
+        private static class Constants {
+            /// <summary>
+            /// Format string for constructing a config file name.
+            /// </summary>
+            public const string ConfigFileNameFormat = "{0}.{1}.config";
+        }
+        #endregion
+
+    }
+}
