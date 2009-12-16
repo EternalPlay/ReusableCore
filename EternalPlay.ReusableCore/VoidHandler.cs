288f7721c0dfc4b10f7c844f01dfba6962a02b33
jdubba <jdubba@jdubba.net>
2009-12-16 17:14:23 -0600

    Added a VoidHandler generic parameterless event handler to the core library.

---
+++ EternalPlay.ReusableCore/VoidHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EternalPlay.ReusableCore {
+    /// <summary>
+    /// Reusable event handler delegate that takes no parameters and returns no value.  Useful for large event driven patterns such as MVC and MVP
+    /// </summary>
+    public delegate void VoidHandler();
+}
